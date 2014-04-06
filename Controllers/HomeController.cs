f72af6e782b52acae9720bcc1aa7256c60846fb5
Hernan Marano <herchugm@gmail.com>
2014-04-06 20:25:19 -0300

    mas archivos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Voodoo.Models;
+
+namespace Voodoo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Sections Sections { get; set; }
+
+        public HomeController(Sections sections)
+        {
+            this.Sections = sections;
+        }
+
+        public ActionResult Index()
+        {
+            return View(this.Sections);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Home()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Trabajos()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Servicios()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Espacio()
+        {
+            var path = this.HttpContext.Server.MapPath(Url.Content("~/Content/Images/Estudio"));
+            var images = Directory.EnumerateFiles(path, "*.jpg");
+            var relatives = images.Select(m => m.Replace(HttpContext.Request.ServerVariables["APPL_PHYSICAL_PATH"], "/").Replace(@"\", "/").Insert(0, "~")).ToList();
+            //relatives.Add("~/Content/images/custom/more-blog.png");
+            return PartialView(relatives);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Nosotros()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Clientes()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Contacto()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Mapa()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView(this.Sections.Where(m => m.ShowInToolbar).ToList());
+        }
+
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static string RelativePath(this HttpServerUtility utility, string path, HttpRequest context)
+        {
+            return path.Replace(context.ServerVariables["APPL_PHYSICAL_PATH"], "/").Replace(@"\", "/");
+        }
+    }
+}
---
+++ Models/Sections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Voodoo.Models
+{
+    public class Sections : List<Section>
+    {
+        public Sections()
+        {
+            this.Add(new Section { Name = "Home", InContainer = false });
+            this.Add(new Section { Name = "Trabajos", Color = "#fff" });
+            this.Add(new Section { Name = "Servicios", Color = "#e5e5e5" });
+            this.Add(new Section { Name = "Espacio", Color = "#333333" });
+            this.Add(new Section { Name = "Nosotros", Color = "#f2f2f2" });
+            this.Add(new Section { Name = "Clientes", Color = "#00B4DA" });
+            this.Add(new Section { Name = "Contacto", Color = "#383838" });
+            this.Add(new Section { Name = "Mapa", InContainer = false, ShowInToolbar = false });
+        }
+    }
+
+    public class Section
+    {
+        public string Name { get; set; }
+        public bool InContainer { get; set; }
+        public string Color { get; set; }
+        public bool ShowInToolbar { get; set; }
+
+        public Section()
+        {
+            this.InContainer = true;
+            this.ShowInToolbar = true;
+        }
+    }
+}
